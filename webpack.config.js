c6606d6ce9e5fdb87ebdc787638f7fdbb903c33b
Hall <hall@stu.pku.edu.cn>
2022-11-07 10:41:58 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Hall\'s App',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
